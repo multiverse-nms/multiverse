3945a253482695d9a883f2a5732ab8ee34bbbdd2
amar-ox <a_abane@hotmail.fr>
2023-07-25 21:17:01 -0400

    first commit

---
+++ prod/run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+export EXTERNAL_IP="127.0.0.1"
+
+# ENV variables to complete:
+# TOKEN_PHRASE, ADMIN_USERNAME, ADMIN_PASSWORD, MYSQL_PASSWORD, B205, GCF
+
+# api-gateway:
+export TOKEN_PHRASE="keyboard cat"
+
+# account:
+export ADMIN_USERNAME="admin"
+export ADMIN_PASSWORD="admin"
+
+# topology:
+export MYSQL_PASSWORD="12345"
+
+# polatis:
+export B205="admin:root"
+export GCF="admin:root"
+
+
+# Get this script directory (to find yml from any directory)
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Stop containers
+docker-compose -f $DIR/docker-compose.yml stop
+
+# Start persistence containers
+docker-compose -f $DIR/docker-compose.yml up -d mysql mongo activemq
+sleep 60
+
+# Start topology service
+docker-compose -f $DIR/docker-compose.yml up -d topology-microservice
+sleep 60
+
+# Start other services
+docker-compose -f $DIR/docker-compose.yml up
